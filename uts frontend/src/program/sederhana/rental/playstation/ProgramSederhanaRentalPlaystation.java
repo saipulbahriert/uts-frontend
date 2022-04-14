ca2949b33b47878b53e20820bf3040ec9bf4a3f1
Saipul Bahri <91308052+saipulbahriert@users.noreply.github.com>
2022-04-14 20:13:36 +0700

    Add files via upload

---
+++ uts frontend/src/program/sederhana/rental/playstation/ProgramSederhanaRentalPlaystation.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program.sederhana.rental.playstation;
+
+/**
+ *
+ * @author Ardy Sendleep
+ */
+public class ProgramSederhanaRentalPlaystation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Frame_rental_playstation Laund = new Frame_rental_playstation();
+        Laund.setVisible(true);
+    }
+}
